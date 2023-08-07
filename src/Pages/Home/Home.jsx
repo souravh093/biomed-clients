4017c9c713af50a4a50fa63ed55764dd31922b20
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-08-08 00:40:28 +0600

    marge conflict resolved

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,21 +2,11 @@ import React from "react";
 import TrendingJobs from "./TrendingJobs/TrendingJobs";
 
 const Home = () => {
-<<<<<<< HEAD
   return (
     <div>
       <TrendingJobs />
     </div>
   );
-=======
-    return (
-        <div>
-            Home
-            This is rakib 
-            <h1>Project starting</h1>
-        </div>
-    );
->>>>>>> 46474746e5d11d360c08d2ca85025f913466373d
 };
 
 export default Home;
