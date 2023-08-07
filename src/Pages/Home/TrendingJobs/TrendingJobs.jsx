625e8807d822a492a4d8ff2504f3d231a01595ee
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-08-08 01:33:22 +0600

    trendingJobs.json file added

--- src/Pages/Home/TrendingJobs/TrendingJobs.jsx
+++ src/Pages/Home/TrendingJobs/TrendingJobs.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../../components/Shared/SectionTitle/SectionTitle";
 
 const TrendingJobs = () => {
-    return (
-        <div>
-            <h2 className='text-2xl text-cyan-600'>Trending Jobs</h2>
-        </div>
-    );
+  return (
+    <div>
+      <SectionTitle
+        heading={"Trending Jobs"}
+        text={"Most viewed and all-time top Jobs"}
+      />
+    </div>
+  );
 };
 
 export default TrendingJobs;
