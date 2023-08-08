99bb51ae93084f56e3f64b1204c2f4200bb4b8dc
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-08-08 10:20:30 +0600

    some issue solved

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
+import Header from '../Pages/Shared/Header/Header';
 
 
 const Root = () => {
     return (
         <div>
-            {/* <Header /> */}
+            <Header />
             <Outlet />
             <Footer />
         </div>
--- src/Pages/Home/TrendingJobs/TrendingJobs.jsx
+++ src/Pages/Home/TrendingJobs/TrendingJobs.jsx
@@ -61,10 +61,10 @@ const TrendingJobs = () => {
             <SwiperSlide className="border rounded-xl" key={job._id} job={job}>
               <img className="rounded-t-xl w-full" src={job.job_image} alt="" />
               <div className="p-2 h-100 overflow-hidden space-y-2">
-                <Link to='/'><h2 className="text-base text-gray-500">{job.job_title}</h2></Link>
-                <p className="mb-1 text-xl font-semibold hover:text-[#5BBB7B] hover:underline duration-500 cursor-pointer">
+                <h2 className="text-base text-gray-500">{job.job_title}</h2>
+                <Link to='/'><p className="mb-1 text-xl font-semibold hover:text-[#5BBB7B] hover:underline duration-500 cursor-pointer">
                   {`${job.job_text.slice(0, 50)}...`}
-                </p>
+                </p></Link>
                 <p className="text-lg">
                   Location:{" "}
                   <span className="text-gray-500">{job.location}</span>
