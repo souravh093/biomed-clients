67b62469f9497ef7380d3bae6835c467707a7138
zabaidulbd <zislam5622@gmail.com>
2023-08-08 13:28:58 +0600

    resolve merge conflict

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import TopCompany from './TopCompany/TopCompany';
 import Categorys from './categorys/Categorys';
-import TrendingJobs from './TrendingJobs/TrendingJobs';
 
 const Home = () => {
     return (
         <div>
-            <TrendingJobs />
             <Categorys />
             <TopCompany />
         </div>
