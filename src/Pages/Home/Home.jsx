61a8abd54c90434b42d73688ae67a1f9fb7ce339
Sourave halder <souravh093@gmail.com>
2023-08-09 15:55:41 +0600

    preference job section almost done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner/Banner';
 import TopCompany from './TopCompany/TopCompany';
 import TrendingJobs from './TrendingJobs/TrendingJobs';
 import Categorys from './categorys/Categorys';
+import PreferenceJobs from './PreferenceJobs/PreferenceJobs';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <TrendingJobs />
             <Categorys />
             <TopCompany />
+            <PreferenceJobs />
         </div>
     );
 };
