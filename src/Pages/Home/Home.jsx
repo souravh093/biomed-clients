fe215c441c72299a522871a1cdb7a5afc47ff0da
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-08-10 02:18:35 +0600

    some changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import AboutUs from './AboutUs/AboutUs';
 import Banner from './Banner/Banner';
 import CategoryMenu from './CategoryMenu/CategoryMenu';
+import FrequentlyAskQuestion from './FrequentlyAskQuestion/FrequentlyAskQuestion';
+import PreferenceJobs from './PreferenceJobs/PreferenceJobs';
 import TopCompany from './TopCompany/TopCompany';
 import TrendingJobs from './TrendingJobs/TrendingJobs';
 import Categorys from './categorys/Categorys';
-<<<<<<< HEAD
-import FrequentlyAskQuestion from './FrequentlyAskQuestion/FrequentlyAskQuestion';
-=======
-import PreferenceJobs from './PreferenceJobs/PreferenceJobs';
->>>>>>> 55587f7dc0ae7e36e623c384240aebdc8695d420
 
 const Home = () => {
     return (
@@ -20,11 +17,8 @@ const Home = () => {
             <Categorys />
             <AboutUs/>
             <TopCompany />
-<<<<<<< HEAD
             <FrequentlyAskQuestion />
-=======
             <PreferenceJobs />
->>>>>>> 55587f7dc0ae7e36e623c384240aebdc8695d420
         </div>
     );
 };
