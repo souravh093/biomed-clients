d072937aa3580a54a6844e5fcc34fa5c5499deb3
Sourave halder <souravh093@gmail.com>
2023-08-14 20:32:09 +0600

    firebase almost done

--- src/Pages/Login/Login/Login.jsx
+++ src/Pages/Login/Login/Login.jsx
@@ -4,8 +4,12 @@ import Container from "../../../components/Shared/Container/Container";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import { useContext } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const Login = () => {
+  const { loginUser, googleLoginUser, resetPassword } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
@@ -13,14 +17,28 @@ const Login = () => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    loginUser(data.email, data.password).then((result) => {
+      console.log(result.user);
+    });
+  };
+
+  const handleGoogle = () => {
+    googleLoginUser().then((result) => {
+      console.log(result.user);
+    });
+  };
+
+  const resetPass = () => {
+    const targetEmail = document.getElementById("email").value;
+    resetPassword(targetEmail);
+    alert("Check you email");
   };
 
   return (
     <Container>
-      <div className="grid grid-cols-2 my-10 items-center">
+      <div className="grid lg:grid-cols-2 my-10 items-center ">
         <div>
-          <div className="mb-10">
+          <div className="mb-10 px-10 lg:px-0">
             <h1 className="text-4xl font-semibold mb-5">Sign in</h1>
             <p>
               If you don’t have an account register <br /> You can{" "}
@@ -30,7 +48,7 @@ const Login = () => {
             </p>
           </div>
 
-          <div className="pr-40">
+          <div className="px-10 lg:px-0 lg:pr-40">
             <form
               onSubmit={handleSubmit(onSubmit)}
               className="flex flex-col gap-5"
@@ -43,7 +61,7 @@ const Login = () => {
                   type="email"
                   id="email"
                   placeholder="Enter your email address"
-                  className="w-full p-2 border-b border-gray-300 rounded"
+                  className="w-full p-2 border-b border-gray-300 rounded outline-none"
                   {...register("email", {
                     required: "Email is required",
                   })}
@@ -61,7 +79,7 @@ const Login = () => {
                   type="password"
                   id="password"
                   placeholder="Enter your password"
-                  className="w-full p-2 border-b border-gray-300 rounded"
+                  className="w-full p-2 border-b border-gray-300 rounded outline-none"
                   {...register("password", {
                     required: "Password is required",
                   })}
@@ -69,12 +87,6 @@ const Login = () => {
                 {errors.password && (
                   <p className="text-red-500">{errors.password.message}</p>
                 )}
-              </div>
-
-              <div className="mb-4 text-right">
-                <a href="#" className="text-primary hover:underline">
-                  Reset Password
-                </a>
               </div>
 
               <div className="flex items-center justify-between">
@@ -87,19 +99,28 @@ const Login = () => {
               </div>
             </form>
 
+            <div className="mb-4 text-right">
+              <div onClick={resetPass} className="text-primary hover:underline">
+                Reset Password
+              </div>
+            </div>
+
             <h2 className="text-center my-10 text-gray-600">
               or continue with
             </h2>
 
             <div className="flex items-center justify-center ">
-              <button className="flex items-center gap-3 border px-10 text-2 py-3 rounded-md hover:bg-primary hover:text-gray-50 transition">
+              <button
+                onClick={handleGoogle}
+                className="flex items-center gap-3 border px-10 text-2 py-3 rounded-md hover:bg-gray-100 hover:text-gray-700 transition"
+              >
                 <FcGoogle />
                 Login with google
               </button>
             </div>
           </div>
         </div>
-        <div className="bg-[#23ccc4]">
+        <div className="bg-[#23ccc4] hidden lg:block">
           <img className="shadow-md bg-cover" src={loginImg} alt="" />
         </div>
       </div>
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -5,8 +5,12 @@ import { Link } from "react-router-dom";
 import { FaAlignJustify } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import logo from "../../../assets/logo.png";
+import { useContext } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import ProfileDropdown from "../../../components/ProfileDropdown/ProfileDropdown";
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
   const [toggle, setToggle] = useState(false);
   return (
     <div className="relative z-10">
@@ -29,14 +33,18 @@ const Header = () => {
               <Link to={"/seller"}>Become a Seller</Link>
             </ul>
             <div>
-              <ul className="flex items-center space-x-6">
-                <MenuItem name={"Login"} path={"/login"} />
-                <span className="bg-primary text-gray-100 px-8 py-3 rounded-md hover:bg-[#4ca068] transition cursor-pointer">
-                  <Link to={"/register"} className="font-medium">
-                    Sign Up
-                  </Link>
-                </span>
-              </ul>
+              {user ? (
+                <ProfileDropdown />
+              ) : (
+                <ul className="flex items-center space-x-6">
+                  <MenuItem name={"Login"} path={"/login"} />
+                  <span className="bg-primary text-gray-100 px-8 py-3 rounded-md hover:bg-[#4ca068] transition cursor-pointer">
+                    <Link to={"/register"} className="font-medium">
+                      Sign Up
+                    </Link>
+                  </span>
+                </ul>
+              )}
             </div>
           </div>
 
@@ -75,11 +83,10 @@ const Header = () => {
             <div>
               <ul className="flex items-center space-x-6">
                 <MenuItem name={"Login"} path={"/login"} />
-                <span className="bg-[#5BBB7B] text-gray-100 px-8 py-3 rounded-md hover:bg-[#4ca068] transition cursor-pointer">
-                  <Link to={"/register"} className="font-medium">
-                    Sign Up
-                  </Link>
-                </span>
+                
+                <Link to={"/register"} className="bg-[#5BBB7B] text-gray-100 font-medium px-8 py-3 rounded-md hover:bg-[#4ca068] transition cursor-pointer">
+                  Sign Up
+                </Link>
               </ul>
             </div>
           </div>
