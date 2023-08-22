402b03b639ae88589e8d48fd0c00639bbe9a1b55
zabaidulbd <zislam5622@gmail.com>
2023-08-22 13:23:57 +0600

    furnish contact us page

--- src/components/Shared/Cover/Cover.jsx
+++ src/components/Shared/Cover/Cover.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Cover = ({ title, text }) => {
   return (
-    <div className="bg-gradient-to-tr from-green-300 via-green-400 to-green-500 py-12 px-4 text-white text-center">
+    <div className="bg-gradient-to-tr from-green-300 via-green-400 to-green-500 py-12 px-4 text-white text-center rounded-sm">
       <h1 className="text-4xl font-bold mb-2">{title}</h1>
       <p className="text-xl">{text}</p>
     </div>
@@ -10,27 +10,3 @@ const Cover = ({ title, text }) => {
 };
 
 export default Cover;
-
-{
-  /* <div className="relative lg:mt-1 mx-auto bg-no-repeat bg-cover h-[600px]">
-<div
-  className="absolute inset-y-52 inset-x-10 md:inset-y-40 md:inset-x-28 lg:inset-y-40 lg:inset-x-56 xl:inset-x-72 2xl:lg:inset-x-[800px] flex items-center justify-center text-center text-neutral-content px-10 py-8 bg-black bg-opacity-40"
-  style={{ fontFamily: "Cinzel, serif" }}
->
-  <div>
-    <h1 className="mb-5 text-3xl md:text-4xl lg:text-6xl font-bold text-white">
-      {title}
-    </h1>
-    <p className="mb-5 text-xs md:text-sm lg:text-lg uppercase text-white">
-      {text}
-    </p>
-  </div>
-</div>
-<div
-  className="bg-cover bg-center h-full w-full"
-  style={{
-    backgroundImage: `url("${bgImg}")`,
-  }}
-></div>
-</div> */
-}
