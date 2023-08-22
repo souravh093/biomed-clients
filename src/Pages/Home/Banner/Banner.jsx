8144995e5c68f41e9ceb0700057300c4d4aed93d
zabaidulbd <zislam5622@gmail.com>
2023-08-22 11:48:06 +0600

    furnish the banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,25 +3,23 @@ import Container from "../../../components/Shared/Container/Container";
 import { Player } from '@lottiefiles/react-lottie-player';
 import SearchForm from "../../Shared/Header/SearchForm/SearchForm";
 
-
 const Banner = () => {
   return (
     <Container>
-      <div className="grid grid-cols-1 md:grid-cols-2 mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-10 items-center">
         <div>
-          <h1 className="md:text-3xl lg:text-6xl text-2xl  py-5 md:mt-4 font-semibold  lg:mt-36 text-slate-700">
-            Hire the best freelancers for any job, online.
-          </h1>
-          <p className="md:my-2 font-semibold  pb-3 ">
-            Millions of people use BIOMED. to turn their ideas into reality.
-          </p>
-          
-          <div>
+          <div className="flex flex-col h-full justify-center">
+            <h1 className="md:text-3xl lg:text-6xl text-2xl py-5 md:mt-4 font-semibold text-slate-700">
+              Hire the best freelancer for any jobs, online.
+            </h1>
+            <p className="md:my-2 font-semibold pb-3">
+              Millions of people use BIOMED. to turn their ideas into reality.
+            </p>
             <SearchForm />
           </div>
         </div>
 
-        <div className="p-2 hidden lg:block">
+        <div className="hidden md:block">
           <Player
             src="banner.json"
             className="player"
