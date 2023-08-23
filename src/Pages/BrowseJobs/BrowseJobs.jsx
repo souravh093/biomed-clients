123647f134afca6d801a6ab478a7f5359122e339
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-08-23 20:55:31 +0600

    Added

--- src/Pages/BrowseJobs/BrowseJobs.jsx
+++ src/Pages/BrowseJobs/BrowseJobs.jsx
@@ -14,26 +14,17 @@ const BrowseJobs = () => {
   }, [jobsSidebarToggle]);
 
   return (
-    <Container className="mb-4">
-      <button
-        onClick={() => setJobsSidebarToggle(true)}
-        className="text-base text-primary hover:text-green-600 flex items-center gap-2 bg-slate-200 hover:bg-slate-300 duration-500 px-6 py-3 rounded-md mt-6 lg:hidden"
-      >
-        <HiOutlineMenuAlt3 /> Menu
-      </button>
-      <div className="grid grid-cols-4 lg:grid-cols-10 xl:grid-cols-4 pt-[20px]">
-        <aside
-          className={`col-span-3 lg:col-span-3 xl:col-span-1 hidden lg:block sticky top-0  h-screen mb-[300px] overflow-y-scroll`}
-          style={{
-            transform: isSidebarOpen ? "translateX(0)" : "translateX(-100%)",
-            transition: "transform 0.3s ease-in-out",
-          }}
+    <div className="pt-20">
+      <Container>
+        <button
+          onClick={() => setJobsSidebarToggle(true)}
+          className="text-base text-primary hover:text-green-600 flex items-center gap-2 bg-slate-200 hover:bg-slate-300 duration-500 px-6 py-3 rounded-md mt-6 lg:hidden"
         >
-          <JobsSidebar />
-        </aside>
-        {jobsSidebarToggle && (
+          <HiOutlineMenuAlt3 /> Menu
+        </button>
+        <div className="grid grid-cols-4 lg:grid-cols-10 xl:grid-cols-4 pt-[20px]">
           <aside
-            className="col-span-3 md:col-span-2 lg:hidden sticky top-0 min-h-screen mb-[400px] overflow-scroll bg-white"
+            className={`col-span-3 lg:col-span-3 xl:col-span-1 hidden lg:block sticky top-0  h-screen mb-[300px] overflow-y-scroll`}
             style={{
               transform: isSidebarOpen ? "translateX(0)" : "translateX(-100%)",
               transition: "transform 0.3s ease-in-out",
@@ -41,12 +32,26 @@ const BrowseJobs = () => {
           >
             <JobsSidebar />
           </aside>
-        )}
-        <div className="col-span-4 lg:col-span-7 xl:col-span-3">
-          <Outlet />
+
+          {jobsSidebarToggle && (
+            <aside
+              className="col-span-3 md:col-span-2 lg:hidden sticky top-0 min-h-screen mb-[400px] overflow-scroll bg-white"
+              style={{
+                transform: isSidebarOpen
+                  ? "translateX(0)"
+                  : "translateX(-100%)",
+                transition: "transform 0.3s ease-in-out",
+              }}
+            >
+              <JobsSidebar />
+            </aside>
+          )}
+          <div className="col-span-4 lg:col-span-7 xl:col-span-3">
+            <Outlet />
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
