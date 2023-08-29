76404d22a4716d8210171b2f32d9a3c27284683e
unknown <kamrulhasananik019@gmail.com>
2023-08-29 23:33:14 +0600

    lazy loader skeleton add on all alication part

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,19 @@ import router from "./Routes/Routes.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./Provider/AuthProvider";
 import { Toaster } from 'react-hot-toast';
+import { SkeletonTheme } from "react-loading-skeleton";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <SkeletonTheme  baseColor="#202020" highlightColor="#444">
     <AuthProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
         <Toaster />
       </QueryClientProvider>
     </AuthProvider>
+    </SkeletonTheme>
   </React.StrictMode>
 );
