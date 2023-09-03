8165b0fa7cf4eb71c9b76c27600dc0d28b0aaf97
zabaidulbd <zislam5622@gmail.com>
2023-09-03 21:26:14 +0600

    add profile image in testimonials

--- src/Pages/Home/PreferenceJobs/PreferenceItem/PreferenceItem.jsx
+++ src/Pages/Home/PreferenceJobs/PreferenceItem/PreferenceItem.jsx
@@ -1,80 +1,31 @@
 import React from "react";
-import { useState } from "react";
-import {
-  FaFacebook,
-  FaTwitter,
-  FaInstagram,
-  FaLinkedin,
-  FaGithub,
-} from 'react-icons/fa';
+import quoteImg from '../../../../assets/quote/quote.png'
 
 const PreferenceItem = ({ task }) => {
   const {
-    task_image,
+    ceo_image,
     ceo_name,
-    task_title
+    task_text
   } = task;
-  const [isHovered, setIsHovered] = useState(false);
-
 
 
   return (
-    <div
-      className={`bg-gray-100 ${isHovered ? '' : 'skew-y-12 -rotate-2'}`}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div
-        className="bg-white shadow-md rounded-lg overflow-hidden w-100"
-      >
-        <div className="p-4 pt-10 rotate-card">
+    <div className="bg-white shadow-md rounded-lg overflow-hidden w-100">
+      <div className="p-4 text-center">
+        <div className="mt-4">
           <img
-            className={`w-48 h-48 rounded-full mx-auto ${isHovered ? '' : 'transform'
-              }`}
-            src={task_image}
-            alt="Profile"
+            className="w-12 h-12 mx-auto mb-2"
+            src={quoteImg}
+            alt="Testimonials Icon"
           />
-          <h2 className="text-xl font-semibold text-center mt-4">{ceo_name}</h2>
-          <p className="text-gray-600 text-center mt-2">{task_title}</p>
+          <h2 className="text-xl font-semibold">{ceo_name}</h2>
+          <p className="text-gray-600 mt-2">{task_text}</p>
         </div>
-        <div className="px-6 py-4">
-          <div className="flex justify-evenly">
-            <div className="text-sm text-gray-600">
-              <span className="font-semibold">Experience</span>
-              <br />
-              <span>1.5 years</span>
-            </div>
-            <div className="text-sm text-gray-600">
-              <span className="font-semibold">Achievement</span>
-              <br />
-              <span>Top-rated evaluator</span>
-            </div>
-          </div>
-        </div>
-        <div className="px-6 py-4">
-          <div className="flex justify-center">
-            <a href="#" className="text-gray-500 mx-3">
-              <FaFacebook />
-            </a>
-            <a href="#" className="text-gray-500 mx-3">
-              <FaTwitter />
-            </a>
-            <a href="#" className="text-gray-500 mx-3">
-              <FaInstagram />
-            </a>
-            <a href="#" className="text-gray-500 mx-3">
-              <FaLinkedin />
-            </a>
-            <a href="#" className="text-gray-500 mx-3">
-              <FaGithub />
-            </a>
-          </div>
-        </div>
-        <div className="px-6 py-4">
-          <button className=" bg-primary text-white font-semibold py-2 px-4 rounded-full w-full hover:bg-green-600 transition-all duration-200">
-            Message
-          </button>
-        </div>
+        <img
+          className="w-16 h-16 mx-auto rounded-full mt-4"
+          src={ceo_image}
+          alt="Profile"
+        />
       </div>
     </div>
   );
